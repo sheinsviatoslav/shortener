c62935ed9d12c0fac9195fb0a94b09db1255f9f0
Svyatoslav Shein <sashein@avito.ru>
2025-03-01 16:28:55 +0300

    add build vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/sheinsviatoslav/shortener/internal/config"
 	"github.com/sheinsviatoslav/shortener/internal/routes"
@@ -9,7 +10,16 @@ import (
 	_ "net/http/pprof"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 	config.Init()
 
 	log.Println("listen on", *config.ServerAddr)
